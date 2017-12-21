78a11f44e8be9129b39c9f5666a2b19d379afa5b
Rohith Rajan <rohithkrajan@hotmail.com>
2017-12-21 10:24:11 +0530

    added a component to components folder

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,23 @@ import * as React from 'react';
 import './App.css';
 
 import { Button } from 'react-bootstrap';
-
+import HomePage from '../src/components/Jumbotron'
 const logo = require('./logo.svg');
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        
+        <p className="App-intro">
+        <Button bsStyle="primary">Hello</Button>
+        <HomePage/>
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-        <Button bsStyle="primary">Hello</Button>
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
       </div>
     );
   }
