a7e815b61889ed24acd78c0c19dd5cbcb1c10cda
Rohith Rajan <rohithkrajan@hotmail.com>
2017-12-22 13:42:37 +0530

    added json-server and concurrently

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import * as React from 'react';
 import './App.css';
 
-import { Button } from 'react-bootstrap';
-import HomePage from '../src/components/Jumbotron'
+//import { Button } from 'react-bootstrap';
+//import HomePage from './components/Jumbotron'
 const logo = require('./logo.svg');
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        
-        <p className="App-intro">
-        <Button bsStyle="primary">Hello</Button>
-        <HomePage/>
+      <div className="App">        
+        <p className="App-intro">        
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
         <div className="App-header">
--- src/components/button.ts
+++ src/components/button.ts
@@ -1,1 +1,3 @@
 //created for the base structure
+
+//import { Navbar, Jumbotron, Button } from 'react-bootstrap';
